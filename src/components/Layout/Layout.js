8631adb617e2eb75a851fdcfc52285c75db77b33
Andrii Kulyk <andrij.kulyk@gmail.com>
2022-10-14 12:08:07 +0300

    updt

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,5 @@
+import { Box } from 'components/Box';
+
+export const Layout = () => {
+  return <Box p={4} display="flex"></Box>;
+};
---
+++ src/theme.js
@@ -0,0 +1,25 @@
+const theme = {
+  colors: {
+    black: '#000',
+    white: '#fff',
+    teal: '#teal',
+  },
+  space: [0, 2, 4, 8, 16, 32, 64, 128, 256],
+  fontSizes: {},
+  fontWeights: {
+    normal: '400',
+    bold: '700',
+  },
+  borders: {
+    none: 'none',
+    normal: '1px solid',
+  },
+  radii: {
+    none: '0',
+    small: '4px',
+    normal: '8px',
+    round: '50%',
+  },
+};
+
+export default theme;
